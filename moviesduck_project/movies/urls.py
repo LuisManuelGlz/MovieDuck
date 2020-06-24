ba2c82401dc763b06df85353caeeb9e1778bdebc
Luis Manuel González <mannyglzg@gmail.com>
2020-06-23 23:53:23 -0700

    Set movie detail format in movie_detail.html

--- moviesduck_project/movies/urls.py
+++ moviesduck_project/movies/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from movies import views
+from django.views.generic import TemplateView # temporal import
 
 app_name = "movies"
 urlpatterns = [
+    path("detail/", TemplateView.as_view(template_name="movies/movie_detail.html"), name="movie_detail"),
     # Carousel-based lists
     path("newest/carousel/", views.CarouselNewReleases.as_view(), name="newest_carousel"),
     path("trending/carousel/", views.CarouselTrending.as_view(), name="trending_carousel"),
