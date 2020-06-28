1972dc532b3227bb948c8baca1b2956953e2d499
Luis Manuel González <mannyglzg@gmail.com>
2020-06-27 23:27:43 -0700

    Fix bug in registration forms

--- moviesduck_project/registration/forms.py
+++ moviesduck_project/registration/forms.py
@@ -18,15 +18,15 @@ class UserCreationFormWithEmail(UserCreationForm):
 
   def clean_first_name(self):
     first_name = self.cleaned_data.get('first_name')
-    if not first_name.isalpha():
+    if not len(first_name) > 0:
       raise forms.ValidationError("I know that's not your first name")
-    return first_name
+    return first_name.strip()
 
   def clean_last_name(self):
     last_name = self.cleaned_data.get('last_name')
-    if not last_name.isalpha():
+    if not len(last_name) > 0:
       raise forms.ValidationError("I know that's not your last name")
-    return last_name
+    return last_name.strip()
 
   def clean_email(self):
     email = self.cleaned_data.get('email')
@@ -54,15 +54,15 @@ class AccountForm(forms.ModelForm):
 
   def clean_first_name(self):
     first_name = self.cleaned_data.get('first_name')
-    if not first_name.isalpha():
-      raise forms.ValidationError('The name not is valid')
-    return first_name
+    if not len(first_name) > 0:
+      raise forms.ValidationError("I know that's not your first name")
+    return first_name.strip()
 
   def clean_last_name(self):
     last_name = self.cleaned_data.get('last_name')
-    if not last_name.isalpha():
-      raise forms.ValidationError('The last name not is valid')
-    return last_name
+    if not len(last_name) > 0:
+      raise forms.ValidationError("I know that's not your last name")
+    return last_name.strip()
 
   def clean_email(self):
     email = self.cleaned_data.get('email')
